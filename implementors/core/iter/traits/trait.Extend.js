5f58a96e40712c353fdbb16625337599d6be66a1
Travis CI User <travis@example.org>
2019-01-15 22:48:56 +0000

    Documentation for nickel-org/nickel.rs@2d0e3d0

---
+++ implementors/core/iter/traits/trait.Extend.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["hyper"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;<a class=\"struct\" href=\"hyper/header/struct.HeaderView.html\" title=\"struct hyper::header::HeaderView\">HeaderView</a>&lt;'a&gt;&gt; for <a class=\"struct\" href=\"hyper/header/struct.Headers.html\" title=\"struct hyper::header::Headers\">Headers</a>",synthetic:false,types:["hyper::header::Headers"]},];
+implementors["serde_json"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">(</a><a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>, <a class=\"enum\" href=\"serde_json/value/enum.Value.html\" title=\"enum serde_json::value::Value\">Value</a><a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.tuple.html\">)</a>&gt; for <a class=\"struct\" href=\"serde_json/map/struct.Map.html\" title=\"struct serde_json::map::Map\">Map</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>, <a class=\"enum\" href=\"serde_json/value/enum.Value.html\" title=\"enum serde_json::value::Value\">Value</a>&gt;",synthetic:false,types:["serde_json::map::Map"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
